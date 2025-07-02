31f2d1338107fea1623790ff74d89fca7e314784
jongwo0k <pygudo@naver.com>
2025-07-03 04:27:16 +0900

    stack

---
+++ cpp/stack.cpp
@@ -0,0 +1,18 @@
+// LIFO - Last In First Out
+#include <stack>
+
+// pair<int, int>, string, ...
+stack<int> sample;
+
+sample.push(1); // 1
+sample.push(3); // 1 3
+sample.push(2); // 1 3 2
+
+// if (!sample.empty())
+sample.top(); // return 2
+
+sample.size(); // return 3
+
+sample.pop(); // delete 2 (1 3)
+
+sample.empty(); // return 0 (X -> 0, O -> 1)
